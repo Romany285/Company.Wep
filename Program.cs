98093b9cf130a6dc5f02c22c8ff014fc1f2fa0d7
romany <romanyghttas332@gmail.com>
2024-10-07 00:47:34 +0300

    employee and department repository and generic repository

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Company.Data.Contexts;
+using Company.Repository.Interfaces;
+using Company.Repository.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -9,15 +11,15 @@ namespace Company.Wep
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<CompanyDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+            builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
