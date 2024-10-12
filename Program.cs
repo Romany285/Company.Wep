bd50e62fd4c561d2260db254f8222dc2789e1a1f
romany <romanyghttas332@gmail.com>
2024-10-12 20:16:01 +0300

    part1OFSession4

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace Company.Wep
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
